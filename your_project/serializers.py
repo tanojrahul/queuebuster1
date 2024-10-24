821501c31b87c17e36707f5ec3a441cd7a042d4e
tanojrahul <137784309+tanojrahul@users.noreply.github.com>
2024-10-24 18:56:38 +0530

    Delete your_project directory

--- your_project/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from your_app.models import PaymentOrder
-
-class PaymentOrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PaymentOrder
-        fields = ['id', 'amount', 'currency', 'status', 'created_at']
